98706593e2962dc0d05f96d578edb0af620cc901
Rupali Natewad <rupalirnatewad@gmail.com>
2022-05-25 21:48:46 +0530

    Add project files.

---
+++ FileIODemo/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FileIODemo;
+
+Console.WriteLine("Hello, World!");
+Demo.FileExists();
+Demo.ReadAllLines();
+Demo.ReadAllText();
+Demo.FileCopy();
+Demo.DeleteFile();
+Demo.ReadFromStreamReader();
+Demo.WriteUsingStreamWriter();
+Console.ReadKey();
---
+++ SerializationDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SerializationDemo;
+
+Console.WriteLine("Hello, World!");
+BinarySerialization obj = new BinarySerialization();
+obj.Serialization();
+BinaryDeSerialization obj1 = new BinaryDeSerialization();
+obj1.DeSerialization();
